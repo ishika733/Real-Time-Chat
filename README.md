aef5202d99c29c0a78517f4d6c0ae824086ec531
ishika733 <ishikabansal5656@gmail.com>
2023-08-31 18:44:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chat-Application
+Realtime chat app with websockets using Node.js, Express and Socket.io with Vanilla JS on the frontend with a custom UI
+
+<h1>Usage</h1>
+npm install
+npm run dev
+
+Go to localhost:3000
---
+++ src/utils/messages.js
@@ -0,0 +1,29 @@
+//
+// ─── MESSAGE FOR TEXT ───────────────────────────────────────────────────────────
+//
+
+var generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+//
+// ─── MESSAGE FOR LOCATION ───────────────────────────────────────────────────────
+
+var generateLocationMessage = (username, url) => {
+    return{
+        username, 
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+// ────────────────────────────────────────────────────────────────────────────────
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
